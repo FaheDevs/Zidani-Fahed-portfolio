c29fa06e2c5570a1d0bd2239f821f76e01a2df74
zidanifahed <fahed-imed.zidani@etu.yniv-amu.fr>
2022-09-17 18:47:19 +0200

    fixed for netifly

--- src/info/Info.js
+++ src/info/Info.js
@@ -29,7 +29,7 @@ of what each of the values mean.
 export const info = {
     firstName: "Fahed",
     lastName: "Zidani",
-    initials: "FiZ", // the example uses first and last, but feel free to use three or more if you like.
+    initials: "FIZ", // the example uses first and last, but feel free to use three or more if you like.
     position: "Master's student in software development",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
